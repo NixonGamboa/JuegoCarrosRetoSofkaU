9e226cab58f67b8e50a5025654feca8c1667edfa
Nixon Gamboa <nrgamboa97@gmail.com>
2021-04-12 12:00:15 -0500

    Inicializando proyecto Spring

---
+++ src/main/java/com/gamboatech/gamecar/GamecarApplication.java
@@ -0,0 +1,13 @@
+package com.gamboatech.gamecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamboatech/gamecar/GamecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamboatech.gamecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
